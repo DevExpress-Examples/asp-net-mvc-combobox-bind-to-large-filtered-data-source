c9d24730db25d78e9daa05ddb179fb85c7325df7
DevExpressExampleBot <fakeEmail>
2023-06-21 00:08:14 +0400

    autogenerate VB

--- VB/ComboBoxLargeDataBase/Controllers/HomeController.vb
+++ VB/ComboBoxLargeDataBase/Controllers/HomeController.vb
@@ -1,15 +1,17 @@
-Imports Microsoft.VisualBasic
 Imports System.Web.Mvc
 Imports ComboBoxLargeDataBase.Model
 
 Namespace ComboBoxLargeDataBase.Controllers
-	Public Class HomeController
-		Inherits Controller
-		Public Function Index() As ActionResult
-			Return View(DataHelper.Persons)
-		End Function
-		Public Function ComboBoxPartial() As ActionResult
-			Return PartialView("ComboBoxPartial", DataHelper.Persons)
-		End Function
-	End Class
+
+    Public Class HomeController
+        Inherits Controller
+
+        Public Function Index() As ActionResult
+            Return View(Persons)
+        End Function
+
+        Public Function ComboBoxPartial() As ActionResult
+            Return PartialView("ComboBoxPartial", Persons)
+        End Function
+    End Class
 End Namespace
